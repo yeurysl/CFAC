41a5350cc36ed3f81057171404e4ec312890a0b7
yeurysl <yeurysl17@gmail.com>
2024-09-23 14:03:23 -0400

    created header. created home, aboutus, and basepage but not structuted

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def base():
+    return render_template('/base.html')
+
+@app.route('/home')
+def home():
+    return render_template('/home.html')
+
+@app.route('/aboutus')
+def about():
+    return render_template('/aboutus.html')
+
+@app.route('/header')
+def header():
+    return render_template('/header.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
