746dc8c6c9b5538bd01f91d3cb4b6eea76f06c50
yeurysl <yeurysl17@gmail.com>
2024-10-04 06:11:27 -0400

    added connection to mongodb, added careerspage, added link to careers in header, created add_user action in app.py, added mongo db and bycrypt

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
 from flask import Flask, render_template
-
+from pymongo import MongoClient
+import bcrypt
 
 app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://yeurys:ZvTt25OmDOp24yCW@cfac.8ba8p.mongodb.net/cfacdb?retryWrites=true&w=majority&appName=cfac", tls=True, tlsAllowInvalidCertificates=True)
+
+db = client["cfacdb"]
+users_collection = db["users"]  # Updated collection to "users"
 
 @app.route('/base')
 def base():
@@ -15,10 +20,33 @@ def home():
 @app.route('/aboutus')
 def about():
     return render_template('/aboutus.html')
+@app.route('/careers')
+def career():
+    return render_template('/careers.html')
 
 @app.route('/header')
 def header():
     return render_template('/header.html')
+
+def add_user(username, password):  # Updated function to add a user
+    # Hash the password
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    
+    # Create a new user document
+    user = {
+        'username': username,
+        'password': hashed_password
+    }
+    
+    # Insert the user into the collection
+    users_collection.insert_one(user)
+    print(f'User {username} added successfully!')
+
+# Use this function to add new users
+if __name__ == '__main__':
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    add_user(username, password)  # Updated to add user instead of employee
 
 if __name__ == '__main__':
     app.run(debug=True)
