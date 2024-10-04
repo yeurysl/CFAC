cd3069b0ae02ba76c03330d536252ae34f19ebf1
yeurysl <yeurysl17@gmail.com>
2024-10-04 07:04:34 -0400

    added login, logout and page protect functions and routes to app.py, created employeepage and login templates

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
-import bcrypt
+from flask_bcrypt import Bcrypt
+from functools import wraps
+import os
+
+
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+bcrypt = Bcrypt(app)
 
 client = MongoClient("mongodb+srv://yeurys:ZvTt25OmDOp24yCW@cfac.8ba8p.mongodb.net/cfacdb?retryWrites=true&w=majority&appName=cfac", tls=True, tlsAllowInvalidCertificates=True)
 
@@ -25,13 +32,50 @@ def about():
 def career():
     return render_template('/careers.html')
 
+@app.route('/employeepage')
+def employeepage():
+    return render_template('/employeepage.html')
+
 @app.route('/header')
 def header():
     return render_template('/header.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password'].encode('utf-8')
+        user = users_collection.find_one({'username': username})
+        
+        if user and bcrypt.check_password_hash(user['password'], password):
+            session['username'] = username
+            return redirect(url_for('employeepage'))  # Ensure 'home' route exists
+        else:
+            return 'Invalid username or password'
+    
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/protected')
+@login_required
+def protected():
+    return "This is a protected page!"
+
 def add_user(username, password):  # Updated function to add a user
     # Hash the password
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
     
     # Create a new user document
     user = {
