0525ed1403c612fa4352db6cd6f67762e3d7fce7
yeurysl <yeurysl17@gmail.com>
2024-10-05 01:23:57 -0400

    Ensured privacy of employeepage by adding login requirement. Gave homepage form functionality to post data. Also added flask message to home page form submission.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
 from functools import wraps
@@ -14,15 +14,52 @@ bcrypt = Bcrypt(app)
 client = MongoClient("mongodb+srv://yeurys:ZvTt25OmDOp24yCW@cfac.8ba8p.mongodb.net/cfacdb?retryWrites=true&w=majority&appName=cfac", tls=True, tlsAllowInvalidCertificates=True)
 
 db = client["cfacdb"]
-users_collection = db["users"]  # Updated collection to "users"
+users_collection = db["users"]  
+estimaterequests_collection = db["estimaterequests"]  
 
 @app.route('/base')
 def base():
     return render_template('/base.html')
 
+
 @app.route('/')
 def home():
     return render_template('/home.html')
+
+
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    address = request.form.get('address')
+    phone = request.form.get('phone')
+
+    if not name or not address or not phone:
+        flash('Please fill out all fields.', 'warning')
+        return redirect(url_for('home'))
+
+    data = {
+        'name': name,
+        'address': address,
+        'phone': phone
+    }
+
+    try:
+        estimaterequests_collection.insert_one(data)
+        print("Data inserted successfully.")
+        flash('Your information has been successfully submitted!', 'success')
+    except Exception as e:
+        print("An error occurred while inserting data:", e)
+        flash('An error occurred while submitting your information. Please try again.', 'danger')
+
+    return redirect(url_for('home'))
+
+
+
+
+
+
 
 @app.route('/aboutus')
 def about():
@@ -32,9 +69,7 @@ def about():
 def career():
     return render_template('/careers.html')
 
-@app.route('/employeepage')
-def employeepage():
-    return render_template('/employeepage.html')
+
 
 @app.route('/header')
 def header():
@@ -87,11 +122,15 @@ def add_user(username, password):  # Updated function to add a user
     users_collection.insert_one(user)
     print(f'User {username} added successfully!')
 
+@app.route('/employeepage')
+@login_required
+def employeepage():
+    return render_template('/employeepage.html')
+
+
 # Use this function to add new users
 if __name__ == '__main__':
     username = input("Enter username: ")
     password = input("Enter password: ")
     add_user(username, password)  # Updated to add user instead of employee
-
-if __name__ == '__main__':
     app.run(debug=True)
