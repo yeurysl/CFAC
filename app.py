71bb008f73b8fa44c4c642fb5cc96520fb88db15
Yeurys <yeurysl17@gmail.com>
2025-02-05 04:41:12 -0500

    added message import

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from config import Config
+from flask_mail import Mail, Message
 
 
 # Import extensions
