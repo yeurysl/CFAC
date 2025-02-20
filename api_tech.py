90a6d8ad931161b12ec6079f821b482005887864
Yeurys <yeurysl17@gmail.com>
2025-02-19 20:02:36 -0500

    lazy rn

--- api_tech.py
+++ api_tech.py
@@ -381,7 +381,6 @@ def notify_techs_for_upcoming_orders():
 
         # Define your query. The $gt operator means "greater than" and $ne means "not equal".
         query = {
-            "service_date": {"$gt": current_time},
             "status": "ordered"  
         }
         current_app.logger.info(f"Executing query: {query}")
