98275f01d864b633db5e797add008534a7a62a9d
yeurysl <147264791+yeurysl@users.noreply.github.com>
2025-06-24 18:29:14 -0400

    Update core.py to accept after payservices

--- blueprints/core.py
+++ blueprints/core.py
@@ -331,7 +331,7 @@ def create_checkout_session():
 
     try:
         session = stripe.checkout.Session.create(
-            payment_method_types=["card"],
+            payment_method_types=["card", "afterpay_clearpay", "klarna"],
             line_items=[{
                 "price": price_id,
                 "quantity": 1,
